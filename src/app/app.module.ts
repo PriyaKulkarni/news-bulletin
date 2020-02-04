a268b9459f3bad9ec45376c760ab6226dd3c613a
PriyaKulkarni <priya.bk18@gmail.com>
2020-02-04 18:39:42 +0530

    HomeComponent added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './modules/material.module';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import {
-  MatInputModule,
   MatButtonModule,
-  MatSelectModule,
   MatIconModule,
   MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
-    MatInputModule,
     MatButtonModule,
-    MatSelectModule,
     MatIconModule,
     MatToolbarModule
   ],
   exports: [
-    MatInputModule,
     MatButtonModule,
-    MatSelectModule,
     MatIconModule,
     MatToolbarModule
   ]
